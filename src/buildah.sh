3574b2e820d697ba4cb837388a96a8ceda977082
Mihai Criveti <crivetimihai@gmail.com>
2021-01-17 18:28:29 +0000

    Buildah script

--- src/buildah.sh
+++ src/buildah.sh
@@ -1,9 +1,23 @@
-container=$(buildah from kubetalks/kubetalks-httpd)
-buildah run ${container} dnf install -y texlive
+# KubeTalks - Build images with Pandoc
+# Mihai Criveti
+
+# Build the container image starting from kubetalks/httpd
+container=$(buildah from quay.io/kubetalks/ubi8)
+
+# Install make and git
+buildah run ${container} dnf install -y make git
+
+# Download pandoc locally and copy it to the image
 wget https://github.com/jgm/pandoc/releases/download/2.9.2/pandoc-2.9.2-linux-amd64.tar.gz -O /tmp/pandoc.tar.gz
 (cd /tmp; tar zxf pandoc.tar.gz)
 buildah copy ${container} /tmp/pandoc-2.9.2/bin /usr/local/bin
+
+# Commit the image
 buildah commit ${container} quay.io/kubetalks/pandoc
-buildah images
+
+# Inspect the new image
+buildah images | grep ${container}
 buildah inspect ${container}
-podman run quay.io/kubetalks/pandoc pandoc --version
+
+# Test that the image works correctly
+podman run --rm quay.io/kubetalks/pandoc pandoc --version
