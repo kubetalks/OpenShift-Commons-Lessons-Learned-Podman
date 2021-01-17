5c83fd32184b1e6d64a92416eb36ff04fa982daf
Mihai Criveti <crivetimihai@gmail.com>
2021-01-17 14:16:01 +0000

    Added pandoc image built using buildah

--- src/buildah.sh
+++ src/buildah.sh
@@ -3,7 +3,7 @@ buildah run ${container} dnf install -y texlive
 wget https://github.com/jgm/pandoc/releases/download/2.9.2/pandoc-2.9.2-linux-amd64.tar.gz -O /tmp/pandoc.tar.gz
 (cd /tmp; tar zxf pandoc.tar.gz)
 buildah copy ${container} /tmp/pandoc-2.9.2/bin /usr/local/bin
-buildah commit ${container} kubetalks/pandoc
+buildah commit ${container} quay.io/kubetalks/pandoc
 buildah images
 buildah inspect ${container}
-podman run kubetalks/pandoc pandoc --version
+podman run quay.io/kubetalks/pandoc pandoc --version
